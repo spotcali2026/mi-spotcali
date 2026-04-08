89999d672e75db460d66d396c7640d8329f7009d
spotcali2026 <spotcali2026@gmail.com>
2026-04-07 19:14:01 -0500

    conexion nueva bd

--- database/mySqlConnection.js
+++ database/mySqlConnection.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const bdmysql = new Sequelize(
-    'bdspotcali',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        port: '3306',
-        dialect: 'mysql'
-    }
+const db = new Sequelize(
+  'u220837946_spotcali_app',
+  'u220837946_spotcali_user',
+  'SpotCali2026',
+  {
+    host: 'mysql.hostinger.com',
+    dialect: 'mysql',
+    port: 3306
+  }
 );
 
 module.exports = {
