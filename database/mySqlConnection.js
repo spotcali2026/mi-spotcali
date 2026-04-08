6f916da13ca0f6c7390dcb414ff41ee5f96fa50d
spotcali2026 <spotcali2026@gmail.com>
2026-04-07 19:56:39 -0500

    fix password bd

--- database/mySqlConnection.js
+++ database/mySqlConnection.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const bdmysql = new Sequelize(
   'u220837946_spotcali_app',
   'u220837946_spotcali_user',
-  'SpotCali2026',
+  'SpotCali2026!',
   {
     host: 'auth-db1978.hstgr.io',
     dialect: 'mysql',
