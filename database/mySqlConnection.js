4c548fcaca4c6c82d274176ebddb31df45501acb
spotcali2026 <spotcali2026@gmail.com>
2026-04-07 19:27:40 -0500

    fix conexion bd

--- database/mySqlConnection.js
+++ database/mySqlConnection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(
+const bdmysql = new Sequelize(
   'u220837946_spotcali_app',
   'u220837946_spotcali_user',
   'SpotCali2026',
